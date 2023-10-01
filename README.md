a58d9977b9c4260444f35577001f78153986cc0d
valentinefleith <valentine.fleith@gmail.com>
2023-10-01 13:23:24 +0200

    add calloc

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ Libft is the first 42's common core's project. It aims to code a C library regro
 - [x] [`ft_strnstr`](/library/ft_strnstr.c)
 - [x] [`ft_atoi`](/library/ft_atoi.c)
 
-- [ ] `ft_calloc`
+- [x] [`ft_calloc`](/library/ft_calloc.c)
 - [x] [`ft_strdup`](/library/ft_strdup.c)
 
 ## Part 2 : Additional functions
---
+++ library/ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vafleith <vafleith@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/01 13:15:37 by vafleith          #+#    #+#             */
+/*   Updated: 2023/10/01 13:22:00 by vafleith         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	if (!count || !size)
+		return (NULL);
+	ptr = malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, count);
+	return (ptr);
+}
