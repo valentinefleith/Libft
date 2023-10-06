786f362dee62431c9f608482063ec8fec6298b18
valentinefleith <valentine.fleith@gmail.com>
2023-10-06 15:14:43 +0200

    first version of itoa unfinished

---
+++ library/ft_itoa.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vafleith <vafleith@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/06 14:20:16 by vafleith          #+#    #+#             */
+/*   Updated: 2023/10/06 15:14:24 by vafleith         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_find_str_size(int n);
+char *ft_uitoa(unsigned int n, char *dest, int size);
+
+char	*ft_itoa(int n)
+{
+	int size_of_new_str;
+	char *result;
+	unsigned int absolute_value;
+
+	size_of_new_str = ft_find_str_size(n);
+	result = malloc(1 + size_of_new_str * sizeof(char));
+	if (result == NULL)
+		return NULL;
+	if (n < 0)
+	{
+		result[0] = '-';
+		absolute_value = n * -1;
+	}
+	else
+		absolute_value = n;
+	return ft_uitoa(absolute_value, result, size_of_new_str);
+}
+
+char *ft_uitoa(unsigned int n, char *dest, int size)
+{
+	int index;
+
+	index = 0;
+	while (index < size)
+	{
+
+	}
+
+		
+}
+
+int ft_find_str_size(int n)
+{
+	int size;
+
+	size = 0;
+	while (n)
+	{
+		n = n / 10;
+		size++;
+	}
+	return size;
+}
