b4ab447d3a876b2a2ff15c63cff330513ecce838
valentinefleith <valentine.fleith@gmail.com>
2023-11-05 01:16:22 +0100

    add malloc protection + correct norm errors

--- library/ft_lstclear.c
+++ library/ft_lstclear.c
@@ -12,9 +12,9 @@
 
 #include "libft.h"
 
-void ft_lstclear(t_list **lst, void (*del)(void *))
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	t_list *next;
+	t_list	*next;
 
 	while (*lst)
 	{
--- library/ft_lstnew.c
+++ library/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: vafleith <vafleith@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 13:40:10 by vafleith          #+#    #+#             */
-/*   Updated: 2023/11/01 13:41:55 by vafleith         ###   ########.fr       */
+/*   Updated: 2023/11/05 01:15:10 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ t_list	*ft_lstnew(void *content)
 	t_list	*node;
 
 	node = malloc(sizeof(t_list *));
+	if (node == NULL)
+		return (node);
 	node->content = content;
 	node->next = NULL;
 	return (node);
