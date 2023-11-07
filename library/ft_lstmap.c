7071acc77060a66964ad5748372be4ad660cf48a
valentinefleith <valentinefleith@users.noreply.github.com>
2023-11-07 17:11:12 +0100

    add unfinished lstmap

--- library/ft_lstmap.c
+++ library/ft_lstmap.c
@@ -6,13 +6,33 @@
 /*   By: vafleith <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 14:48:06 by vafleith          #+#    #+#             */
-/*   Updated: 2023/11/07 14:49:22 by vafleith         ###   ########.fr       */
+/*   Updated: 2023/11/07 17:09:50 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *). void (*del)(void *))
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list *new_lst;
+	t_list *start;
 
+	new_lst = ft_lstnew(lst->content);
+	if (new_lst == NULL)
+		return NULL;
+	start = new_lst;
+
+	while(lst->next)
+	{
+		(*f)(new_lst->content);
+		lst = lst->next;
+		new_lst->next = ft_lstnew(lst->next->content);
+		
+		if (new_lst == NULL)
+		{
+			ft_lstclear(&start, (del));
+			return NULL;
+		}
+	}
+	return start;
 }
