a88fec707a63941ae937c76456e108abe6d91792
valentinefleith <valentine.fleith@gmail.com>
2023-11-13 22:15:11 +0100

    update strrchr

--- library/ft_strrchr.c
+++ library/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: vafleith <vafleith@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 16:33:56 by vafleith          #+#    #+#             */
-/*   Updated: 2023/11/13 21:25:47 by vafleith         ###   ########.fr       */
+/*   Updated: 2023/11/13 22:12:41 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	size_t	i;
+	int	i;
 
 	i = ft_strlen(s) - 1;
-	while (i != 0 && s[i] != c)
+	while (i >= 0 && s[i] != c)
 		i--;
-	if (i)
-		return ((char *)(&s[i]));
+	if (s[i] == c)
+		return ((char *)(s + i));
 	return (NULL);
 }
 /*
