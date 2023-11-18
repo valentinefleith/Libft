6e546501588535f72f4444c815bc38abbe1a954b
valentinefleith <valentinefleith@users.noreply.github.com>
2023-11-18 17:56:27 +0100

    add forgotten i incrementation

--- library/ft_memchr.c
+++ library/ft_memchr.c
@@ -6,23 +6,25 @@
 /*   By: vafleith <vafleith@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 11:41:17 by vafleith          #+#    #+#             */
-/*   Updated: 2023/09/29 14:06:52 by vafleith         ###   ########.fr       */
+/*   Updated: 2023/11/18 17:44:17 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t	i;
-	char	*bytes;
+	unsigned char	*bytes;
 
 	i = 0;
-	bytes = (char *)s;
-	while (bytes[i] && i < n)
+	bytes = (unsigned char *)s;
+	while (i < n)
 	{
-		if (bytes[i] == c)
-			return (&bytes[i]);
+		if (bytes[i] == (unsigned char)c)
+			return (bytes + i);
+		i++;
 	}
 	return (NULL);
 }
