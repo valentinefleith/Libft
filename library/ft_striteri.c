a6d160e95006c5cd19f6c81bdb9ccf91ecfffeeb
valentinefleith <valentinefleith@users.noreply.github.com>
2023-11-18 18:36:11 +0100

    changing syntax errors

--- library/ft_striteri.c
+++ library/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: vafleith <vafleith@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 11:16:17 by vafleith          #+#    #+#             */
-/*   Updated: 2023/11/01 11:20:10 by vafleith         ###   ########.fr       */
+/*   Updated: 2023/11/18 18:33:06 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,11 @@
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	i;
-	unsigned int	s_length;
 
 	i = 0;
-	s_length = ft_strlen(s);
-	while (i < s_length)
+	while (s[i])
 	{
-		(*f)(i, &s[i]);
+		(*f)(i, s + i);
 		i++;
 	}
 }
