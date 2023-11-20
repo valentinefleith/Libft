d4c09e16f8335c359acc074a227d182133bcba81
valentinefleith <valentine.fleith@gmail.com>
2023-11-20 22:48:15 +0100

    add condition if NUL str is given

--- library/ft_atoi.c
+++ library/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: val <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 16:46:39 by val               #+#    #+#             */
-/*   Updated: 2023/11/13 23:13:12 by vafleith         ###   ########.fr       */
+/*   Updated: 2023/11/20 22:47:42 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ static unsigned int	ft_atoui(const char *str);
 
 int	ft_atoi(const char *str)
 {
+	if (!*str)
+		return (0);
 	while (ft_strchr(" \t\n\r\v\f", *str))
 		str++;
 	if (*str == '-')
@@ -40,11 +42,12 @@ static unsigned int	ft_atoui(const char *str)
 	}
 	return (output);
 }
+
 /*
 #include <stdio.h>
 
 int	main(void)
 {
-	printf("%i\n", ft_atoi("   			\n-42fddfj"));
+	printf("%i\n", ft_atoi("\0"));
 }
 */
