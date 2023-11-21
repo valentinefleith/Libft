2676890ec82a5b899f3666e30cab1b4573dc9feb
valentinefleith <valentine.fleith@gmail.com>
2023-11-21 23:21:37 +0100

    delete useless condition in while loop

--- library/ft_memcmp.c
+++ library/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: vafleith <vafleith@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 23:32:04 by vafleith          #+#    #+#             */
-/*   Updated: 2023/11/21 23:06:56 by vafleith         ###   ########.fr       */
+/*   Updated: 2023/11/21 23:14:25 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	c_s2 = s2;
 	if (n == 0)
 		return (0);
-	while (i < n - 1 && c_s1[i] == c_s2[i] && c_s1[i] && c_s2[i])
+	while (i < n - 1 && c_s1[i] == c_s2[i])
 		i++;
 	return (c_s1[i] - c_s2[i]);
 }
