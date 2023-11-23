9148fdd479f25e17719916edcb939aee1e68cece
valentinefleith <valentine.fleith@gmail.com>
2023-11-23 10:12:19 +0100

    update version

--- library/ft_strtrim.c
+++ library/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: vafleith <vafleith@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 17:19:03 by vafleith          #+#    #+#             */
-/*   Updated: 2023/10/05 17:59:02 by vafleith         ###   ########.fr       */
+/*   Updated: 2023/11/23 10:12:00 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 static size_t	ft_get_size_of_trimmed(char const *s1, char const *set);
 static void		ft_strtrcpy(char *dest, char const *s1, char const *set);
+static int ft_to_be_trimmed(int i);
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*trimmed;
 
-	trimmed = malloc(ft_get_size_of_trimmed(s1, set) * sizeof(char));
+	trimmed = malloc(1 + ft_get_size_of_trimmed(s1, set) * sizeof(char));
 	if (trimmed == NULL)
 		return (NULL);
 	ft_strtrcpy(trimmed, s1, set);
@@ -37,20 +38,24 @@ static size_t	ft_get_size_of_trimmed(char const *s1, char const *set)
 			size++;
 		s1++;
 	}
-	return (1 + size);
+	return (size);
 }
 
-static void	ft_strtrcpy(char *dest, char const *s1, char const *set)
+static void	ft_strtrcpy(char *dest, char const *s1, char const *set) // I should use ft_substr here
 {
-	while (*s1)
+	int i;
+
+	i = 0;
+	while (s1[i])
 	{
-		if (!ft_strchr(set, *s1))
+		if (!ft_strchr(set, s1[i]) && ft_to_be_trimmed(i))
 		{
-			*dest = *s1;
+			*dest = s1[i];
 			dest++;
 		}
-		s1++;
+		i++;
 	}
+	*dest = '\0';
 }
 
 /*
