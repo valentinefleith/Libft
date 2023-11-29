e5e17e7523584b80360eaaf8123cea58e27f8cfe
valentinefleith <valentine.fleith@gmail.com>
2023-11-29 11:05:32 +0100

    fix function

--- library/ft_lstmap.c
+++ library/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: vafleith <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 14:48:06 by vafleith          #+#    #+#             */
-/*   Updated: 2023/11/28 16:27:50 by vafleith         ###   ########.fr       */
+/*   Updated: 2023/11/29 11:05:03 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,23 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_lst;
 	t_list	*start;
 
-	new_lst = ft_lstnew(lst->content);
+	new_lst = ft_lstnew((*f)(lst->content));
 	if (new_lst == NULL)
 		return (NULL);
 	start = new_lst;
-	while (lst->next)
+	lst = lst->next;
+	while (lst)
 	{
 		(*f)(new_lst->content);
-		lst = lst->next;
-		new_lst->next = ft_lstnew(lst->content);
+		new_lst->next = ft_lstnew((*f)(lst->content));
 		if (new_lst->next == NULL)
 		{
 			ft_lstclear(&start, (del));
 			return (NULL);
 		}
 		new_lst = new_lst->next;
+		lst = lst->next;
 	}
+	new_lst->next = NULL;
 	return (start);
 }
