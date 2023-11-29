4115dcf69b24d329c909d01c90efd294271ae287
valentinefleith <valentine.fleith@gmail.com>
2023-11-29 12:14:27 +0100

    fix strnstr

--- library/ft_strnstr.c
+++ library/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: vafleith <vafleith@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 16:20:57 by vafleith          #+#    #+#             */
-/*   Updated: 2023/11/29 10:39:10 by vafleith         ###   ########.fr       */
+/*   Updated: 2023/11/29 12:13:12 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	needle_length;
-	
+
 	needle_length = ft_strlen(needle);
 	if (!needle_length)
 		return ((char *)haystack);
 	i = 0;
-	while (haystack[i] && i + needle_length < len)
+	while (haystack[i] && i + needle_length <= len)
 	{
 		if (!ft_strncmp((char *)(haystack + i), needle, needle_length))
 		{
@@ -31,3 +31,18 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	}
 	return (NULL);
 }
+/*
+#include <stdio.h>
+int main(void)
+{
+	char haystack[30] = "abcdef";
+	char *result = ft_strnstr(haystack, "ef", 5);
+	if (result == NULL)
+		printf("OK\n");
+	else
+	{
+		printf("rip\n");
+		printf("the function returned %s instead of null\n", result);
+		}
+}
+*/
