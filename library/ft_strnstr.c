3dffc17b493c25f1c732212eaed8e41730c05e7a
valentinefleith <valentine.fleith@gmail.com>
2023-11-29 10:40:04 +0100

    correct little mistake

--- library/ft_strnstr.c
+++ library/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: vafleith <vafleith@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 16:20:57 by vafleith          #+#    #+#             */
-/*   Updated: 2023/11/23 09:30:53 by vafleith         ###   ########.fr       */
+/*   Updated: 2023/11/29 10:39:10 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	needle_length;
-
+	
 	needle_length = ft_strlen(needle);
+	if (!needle_length)
+		return ((char *)haystack);
 	i = 0;
 	while (haystack[i] && i + needle_length < len)
 	{
