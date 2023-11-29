7b2a81b6c6897db1700753157442f606536f63e6
valentinefleith <valentine.fleith@gmail.com>
2023-11-29 10:55:18 +0100

    correct little mistake

--- library/ft_lstnew.c
+++ library/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: vafleith <vafleith@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 13:40:10 by vafleith          #+#    #+#             */
-/*   Updated: 2023/11/05 01:15:10 by vafleith         ###   ########.fr       */
+/*   Updated: 2023/11/29 10:54:46 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*node;
 
-	node = malloc(sizeof(t_list *));
+	node = malloc(sizeof(t_list));
 	if (node == NULL)
 		return (node);
 	node->content = content;
